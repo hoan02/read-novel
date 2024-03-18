5905a0c6ef509e0d74485b94422fa2a7c3fd6987
hoan02 <hoanvipboi1@gmail.com>
2024-03-18 22:21:11 +0700

    update base url

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://read-novel-phi.vercel.app/api/",
-  // baseURL: "http://localhost:3000/api/",
+  baseURL: `${process.env.BASE_URL}api/` || "http://localhost:3000/api/",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
